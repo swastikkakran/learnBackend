1f0af1bd3946619d64831c81a6a938b9cbcebbd5
Swastik Kakran <kakranswastik2007@gmail.com>
2026-01-14 12:48:07 +0530

    Created a basic express.js app

--- src/index.js
+++ src/index.js
@@ -1,7 +1,17 @@
 import dotenv from "dotenv"
+import express from "express"
 
 dotenv.config({
     path:"./.env"
 })
 
-console.log("testing...")
+const app = express()
+const port = process.env.PORT || 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})

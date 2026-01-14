3508854655c86fb89b26e49ee7fae097d4174237
Swastik Kakran <kakranswastik2007@gmail.com>
2026-01-14 15:42:28 +0530

    configured basic express and cors

--- src/app.js
+++ src/app.js
@@ -1,6 +1,20 @@
 import express from "express"
+import cors from "cors"
 
 const app = express()
+
+//basic configuration middlewares
+app.use(express.json({ limit:"16kb" }))
+app.use(express.urlencoded({ extended:true, limit:"16kb" }))
+app.use(express.static("public"))
+
+//cors configurations
+app.use(cors({
+    origin:process.env.CORS_ORIGIN?.split(","),
+    credentials:true,
+    methods:["GET", "POST", "DELETE", "PATCH", "OPTIONS", "PUT"],
+    allowedHeaders:["Authorization", "Content-Type"]
+}))
 
 app.get('/', (req, res) => {
   res.send('Hello World!')
--- src/index.js
+++ src/index.js
@@ -8,5 +8,5 @@ dotenv.config({
 const port = process.env.PORT || 3000
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
+  console.log(`Example app listening on http://localhost:${port}`)
 })

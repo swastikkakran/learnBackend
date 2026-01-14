56c6b971501f4b0de7fbb82eb23c6cc9c2699ff4
Swastik Kakran <kakranswastik2007@gmail.com>
2026-01-14 13:12:23 +0530

    Created app.js for seperation of experss code from app entry point

--- src/index.js
+++ src/index.js
@@ -1,16 +1,11 @@
 import dotenv from "dotenv"
-import express from "express"
+import app from "./app.js"
 
 dotenv.config({
     path:"./.env"
 })
 
-const app = express()
 const port = process.env.PORT || 3000
-
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)

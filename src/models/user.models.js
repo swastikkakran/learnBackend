daf464197ce1e8c3f0a9fc7014e7d3f3e8734815
Swastik Kakran <kakranswastik2007@gmail.com>
2026-01-19 08:21:16 +0530

    Added password hashing in user schema

--- src/models/user.models.js
+++ src/models/user.models.js
@@ -1,4 +1,5 @@
 import mongoose, { Schema } from "mongoose";
+import bcrypt from "bcrypt";
 
 userSchema = new Schema(
     {
@@ -61,4 +62,11 @@ userSchema = new Schema(
     }
 )
 
-export const User = mongoose.model("User", userSchema)
+userSchema.pre("save", async function (next) {
+    if (!this.modified("password")) return next()
+
+    this.password = await bcrypt.hash(this.password, 10)
+    next();
+});
+
+export const User = mongoose.model("User", userSchema);
